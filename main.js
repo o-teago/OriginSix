5937fd301335a529d266e712d3a6b484b833db48
th-ofc <th.hodz@icloud.com>
2022-02-23 23:27:57 -0300

    contact section

--- main.js
+++ main.js
@@ -37,3 +37,20 @@ const swiper = new Swiper('.swiper-container', {
     keyboard: true
 
 })
+
+// scroll reveal
+
+const scrollReveal = ScrollReveal({
+    origin: 'top',
+    distance: '30px',
+    duration: 700,
+    reset: true
+})
+
+scrollReveal.reveal(
+    `#home .image, #home .text,
+    #about .image, #about .text,
+    #services header, #services .card,
+    #testimonials header, #testimonials .testimonials,
+    #contact .text, #contact ul li`, { interval: 100}
+)
